2c5f483167bff3fffcee0dae2029f6a1250858ad
jinwook song <wlsdnr129@naver.com>
2024-12-15 19:05:25 +0900

    fix: compileSDK 35

--- HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             HelloAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Kotlin",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HelloAndroidTheme {
-        Greeting("Compose")
+        Greeting("Android")
     }
 }
--- HelloAndroid/settings.gradle.kts
+++ HelloAndroid/settings.gradle.kts
@@ -19,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Hello Android"
+rootProject.name = "HelloAndroid"
 include(":app")
