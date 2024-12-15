067a71262e3fdab4c8a6d02260e563fa23c500ea
jinwook song <wlsdnr129@naver.com>
2024-12-15 20:08:53 +0900

    style: with modifier

--- HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.helloandroid.ui.theme.HelloAndroidTheme
 
@@ -34,6 +40,13 @@ fun MyApp() {
                 name = "Android",
                 modifier = Modifier.padding(innerPadding)
             )
+
+            MyFirstImage(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize()
+                    .size(100.dp)
+            )
         }
     }
 }
@@ -44,6 +57,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         fontSize = 25.sp, // scalable pixels
         color = Color.Red,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun MyFirstImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.avatar), // 이미지 리소스 ID
+        contentDescription = "Avatar Image", // 접근성 설명
+        contentScale = ContentScale.Crop,
         modifier = modifier
     )
 }
