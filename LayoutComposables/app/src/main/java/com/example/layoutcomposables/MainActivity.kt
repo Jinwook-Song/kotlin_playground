96deff82536019f7e8c60fdd827a4a2f15f281a1
jinwook song <wlsdnr129@naver.com>
2024-12-17 19:44:01 +0900

    style(layout): Box

--- LayoutComposables/app/src/main/java/com/example/layoutcomposables/MainActivity.kt
+++ LayoutComposables/app/src/main/java/com/example/layoutcomposables/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -77,6 +79,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             )
             Text(
                 text = "Home"
+            )
+        }
+        Box(
+            modifier = modifier
+                .background(Color.Yellow)
+                .size(200.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Icon(
+                Icons.Default.Home,
+                contentDescription = null,
+                modifier = modifier.size(40.dp)
+            )
+            Icon(
+                Icons.Default.Delete,
+                contentDescription = null,
+                modifier = modifier.size(40.dp).align(Alignment.Center)
             )
         }
     }
