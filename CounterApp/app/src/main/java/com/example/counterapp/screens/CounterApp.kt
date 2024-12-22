c0f1965d40f85fc1b1f9c1883b24ba4e8494fbbb
jinwook song <wlsdnr129@naver.com>
2024-12-23 00:41:04 +0900

    feat: recompose, remember, mutableStateOf

--- CounterApp/app/src/main/java/com/example/counterapp/screens/CounterApp.kt
+++ CounterApp/app/src/main/java/com/example/counterapp/screens/CounterApp.kt
@@ -10,26 +10,33 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+
 @Composable
 fun CounterApp(modifier: Modifier = Modifier) {
-    var counter = 0
+    var counter: MutableState<Int> = remember {
+        mutableStateOf(0)
+    }
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Log.d("CounterApp", "CounterApp recomposed")
         Text(text = "You have pushed the button this many times")
-        Text(text = "$counter", style = MaterialTheme.typography.headlineLarge)
+        Text(text = "${counter.value}", style = MaterialTheme.typography.headlineLarge)
         Spacer(modifier = Modifier.height(16.dp))
         Button(
             onClick = {
-                Log.d("CounterApp", "CounterApp: $counter")
-                counter++
+                Log.d("CounterApp", "CounterApp: ${counter.value}")
+                counter.value++
             },
         ) {
             Text("Click me")
