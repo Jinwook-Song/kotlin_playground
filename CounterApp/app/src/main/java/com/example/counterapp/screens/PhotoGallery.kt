80f7a1accf5311314dbd41116c8d42f428eab7a1
jinwook song <wlsdnr129@naver.com>
2024-12-23 19:52:58 +0900

    feat(init): photo gallery

---
+++ CounterApp/app/src/main/java/com/example/counterapp/screens/PhotoGallery.kt
@@ -0,0 +1,18 @@
+package com.example.counterapp.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.counterapp.MyApp
+
+@Composable
+fun PhotoGallery(modifier: Modifier = Modifier) {
+    
+}
+
+
+@Preview
+@Composable
+private fun PhotoGalleryPreview() {
+    MyApp()
+}
