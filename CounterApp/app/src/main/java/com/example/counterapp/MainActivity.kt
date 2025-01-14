201703acc323ab3dc87a593e0924fd70fb620938
jinwook song <wlsdnr129@naver.com>
2025-01-14 22:27:18 +0900

    feat: lifecycles

--- CounterApp/app/src/main/java/com/example/counterapp/MainActivity.kt
+++ CounterApp/app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.counterapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,8 +11,11 @@ import com.example.counterapp.screens.CounterApp
 import com.example.counterapp.ui.theme.CounterAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    val TAG = "ACTIVITY LIFECYCLES"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate called")
         enableEdgeToEdge()
         setContent {
             CounterAppTheme {
@@ -19,6 +23,37 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+
 }
 
 @Composable
