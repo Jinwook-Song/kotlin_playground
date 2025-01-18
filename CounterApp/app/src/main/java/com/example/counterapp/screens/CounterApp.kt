aa569cb0cec4d5de342f19d863d334435277b0d5
jinwook song <wlsdnr129@naver.com>
2025-01-18 15:25:04 +0900

    feat(viewmodel): counter view model

--- CounterApp/app/src/main/java/com/example/counterapp/screens/CounterApp.kt
+++ CounterApp/app/src/main/java/com/example/counterapp/screens/CounterApp.kt
@@ -10,20 +10,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.counterapp.viewmodels.CounterViewModel
 
 
 @Composable
 fun CounterApp(modifier: Modifier = Modifier) {
-    val counter: MutableState<Int> = rememberSaveable {
-        mutableIntStateOf(0)
-    }
+    val counterViewModel: CounterViewModel = viewModel()
+
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -31,12 +29,12 @@ fun CounterApp(modifier: Modifier = Modifier) {
     ) {
         Log.d("CounterApp", "CounterApp recomposed")
         Text(text = "You have pushed the button this many times")
-        Text(text = "${counter.value}", style = MaterialTheme.typography.headlineLarge)
+        Text(text = "${counterViewModel.counter}", style = MaterialTheme.typography.headlineLarge)
         Spacer(modifier = Modifier.height(16.dp))
         Button(
             onClick = {
-                Log.d("CounterApp", "CounterApp: ${counter.value}")
-                counter.value++
+                Log.d("CounterApp", "CounterApp: ${counterViewModel.counter}")
+                counterViewModel.increaseCounter()
             },
         ) {
             Text("Click me")
