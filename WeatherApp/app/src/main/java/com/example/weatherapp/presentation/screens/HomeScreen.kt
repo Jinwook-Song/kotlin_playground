2e1276d5039b2262fb5164c42075b39952b96c2b
jinwook song <wlsdnr129@naver.com>
2025-01-19 17:35:37 +0900

    style(ui): app background

--- WeatherApp/app/src/main/java/com/example/weatherapp/presentation/screens/HomeScreen.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/presentation/screens/HomeScreen.kt
@@ -1,9 +1,50 @@
 package com.example.weatherapp.presentation.screens
 
+import android.provider.CalendarContract.Colors
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.weatherapp.R
+import com.example.weatherapp.presentation.widgets.AppBackground
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        AppBackground(photoId = R.drawable.weather_bg)
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("Weather App", style = MaterialTheme.typography.titleLarge)
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.Transparent,
+                        actionIconContentColor = Color.White
+                    )
+                )
+            },
+            containerColor = Color.Transparent
+        ) {
+            Surface(
+                color = Color.Transparent,
+                modifier = Modifier.padding(it).fillMaxSize().wrapContentSize()
+            ) {
+                Text("Weather Home", style = MaterialTheme.typography.displaySmall)
 
+            }
+        }
+
+    }
 }
