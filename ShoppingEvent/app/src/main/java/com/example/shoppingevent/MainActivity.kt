7ee3ae94386a1924c265587d3631a44d5dea9ff3
jinwook song <wlsdnr129@naver.com>
2025-02-04 22:09:02 +0900

    feat(navigate): destination object with serializable

--- ShoppingEvent/app/src/main/java/com/example/shoppingevent/MainActivity.kt
+++ ShoppingEvent/app/src/main/java/com/example/shoppingevent/MainActivity.kt
@@ -9,6 +9,11 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.shoppingevent.destinations.AddEventRoute
+import com.example.shoppingevent.destinations.EventDetailsRoute
+import com.example.shoppingevent.destinations.HomeRoute
+import com.example.shoppingevent.ui.addevent.AddEventPage
+import com.example.shoppingevent.ui.eventdetails.EventDetailsPage
 import com.example.shoppingevent.ui.home.HomePage
 import com.example.shoppingevent.ui.theme.ShoppingEventTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,10 +36,24 @@ fun ShoppingApp(modifier: Modifier = Modifier) {
         val navController = rememberNavController()
         NavHost(
             navController = navController,
-            startDestination = "home"
+            startDestination = HomeRoute
         ) {
-            composable("home") {
+            composable<HomeRoute>() {
                 HomePage(modifier)
+            }
+            composable<AddEventRoute>() {
+                AddEventPage(
+                    navigateBack = { navController.popBackStack() },
+                    navigateUp = { navController.navigateUp() },
+                    modifier = modifier
+                )
+            }
+            composable<EventDetailsRoute>() {
+                EventDetailsPage(
+                    navigateBack = { navController.popBackStack() },
+                    navigateUp = { navController.navigateUp() },
+                    modifier = modifier
+                )
             }
 
         }
---
+++ ShoppingEvent/app/src/main/java/com/example/shoppingevent/destinations/destinationRoutes.kt
@@ -0,0 +1,13 @@
+package com.example.shoppingevent.destinations
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+object HomeRoute
+
+@Serializable
+object AddEventRoute
+
+@Serializable
+object EventDetailsRoute
