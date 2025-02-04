9ce271c01702c5ba806715be5d09e43ce20d563a
jinwook song <wlsdnr129@naver.com>
2025-02-04 21:58:01 +0900

    feat(navigate): navController, destinaion

--- ShoppingEvent/app/src/main/java/com/example/shoppingevent/MainActivity.kt
+++ ShoppingEvent/app/src/main/java/com/example/shoppingevent/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.shoppingevent.ui.home.HomePage
 import com.example.shoppingevent.ui.theme.ShoppingEventTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,30 +20,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ShoppingEventTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ShoppingApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun ShoppingApp(modifier: Modifier = Modifier) {
     ShoppingEventTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = "home"
+        ) {
+            composable("home") {
+                HomePage(modifier)
+            }
+
+        }
+
     }
 }
+
+
