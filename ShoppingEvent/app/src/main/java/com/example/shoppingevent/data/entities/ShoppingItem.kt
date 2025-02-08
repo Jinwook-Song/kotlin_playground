e6d5e29e742605936bd7c3b44540deb13fce4ecd
jinwook song <wlsdnr129@naver.com>
2025-02-08 22:29:38 +0900

    feat(dao): define DAOs for entity

--- ShoppingEvent/app/src/main/java/com/example/shoppingevent/data/entities/ShoppingItem.kt
+++ ShoppingEvent/app/src/main/java/com/example/shoppingevent/data/entities/ShoppingItem.kt
@@ -1,9 +1,21 @@
 package com.example.shoppingevent.data.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items")
 data class ShoppingItem(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "item_id")
     val itemId: Long = 0,
+
+    @ColumnInfo(name = "event_id")
     val eventId: Long,
+
+    @ColumnInfo(name = "item_name")
     val itemName: String,
+
     val price: Double = 0.0,
     val quantity: Double = 1.0,
 )
