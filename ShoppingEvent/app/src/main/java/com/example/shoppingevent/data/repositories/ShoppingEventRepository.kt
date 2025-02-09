7c68f9c0fdffb742ec54d91045a29936fad9ebd7
jinwook song <wlsdnr129@naver.com>
2025-02-09 23:06:23 +0900

    feat(repository): shopping repository

--- ShoppingEvent/app/src/main/java/com/example/shoppingevent/data/repositories/ShoppingEventRepository.kt
+++ ShoppingEvent/app/src/main/java/com/example/shoppingevent/data/repositories/ShoppingEventRepository.kt
@@ -1,4 +1,30 @@
 package com.example.shoppingevent.data.repositories
 
-class ShoppingEventRepository {
+import com.example.shoppingevent.data.daos.ShoppingEventDao
+import com.example.shoppingevent.data.entities.ShoppingEvent
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface ShoppingEventRepository {
+    suspend fun insert(shoppingEvent: ShoppingEvent)
+    suspend fun update(shoppingEvent: ShoppingEvent)
+    suspend fun delete(shoppingEvent: ShoppingEvent)
+    fun getEvents(): Flow<List<ShoppingEvent>>
+}
+
+class ShoppingEventRepositoryImpl @Inject constructor(
+    private val shoppingEventDao: ShoppingEventDao,
+) : ShoppingEventRepository {
+    override suspend fun insert(shoppingEvent: ShoppingEvent) =
+        shoppingEventDao.insert(shoppingEvent)
+
+    override suspend fun update(shoppingEvent: ShoppingEvent) =
+        shoppingEventDao.update(shoppingEvent)
+
+    override suspend fun delete(shoppingEvent: ShoppingEvent) =
+        shoppingEventDao.delete(shoppingEvent)
+
+    override fun getEvents(): Flow<List<ShoppingEvent>> =
+        shoppingEventDao.getEvents()
+
 }
---
+++ ShoppingEvent/app/src/main/java/com/example/shoppingevent/data/repositories/ShoppingItemRepository.kt
@@ -0,0 +1,29 @@
+package com.example.shoppingevent.data.repositories
+
+import com.example.shoppingevent.data.daos.ShoppingItemDao
+import com.example.shoppingevent.data.entities.ShoppingItem
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface ShoppingItemRepository {
+    suspend fun insert(shoppingItem: ShoppingItem)
+    suspend fun update(shoppingItem: ShoppingItem)
+    suspend fun delete(shoppingItem: ShoppingItem)
+    fun getItems(): Flow<List<ShoppingItem>>
+}
+
+class ShoppingItemRepositoryImpl @Inject constructor(
+    private val shoppingItemDao: ShoppingItemDao,
+) : ShoppingItemRepository {
+    override suspend fun insert(shoppingItem: ShoppingItem) =
+        shoppingItemDao.insert(shoppingItem)
+
+    override suspend fun update(shoppingItem: ShoppingItem) =
+        shoppingItemDao.update(shoppingItem)
+
+    override suspend fun delete(shoppingItem: ShoppingItem) =
+        shoppingItemDao.delete(shoppingItem)
+
+    override fun getItems(): Flow<List<ShoppingItem>> =
+        shoppingItemDao.getItems()
+}
