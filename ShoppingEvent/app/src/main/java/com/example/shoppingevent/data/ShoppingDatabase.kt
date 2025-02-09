cc2ab2e22d058907606a74974105f64cb8358d21
jinwook song <wlsdnr129@naver.com>
2025-02-09 21:52:42 +0900

    feat(database): create database

---
+++ ShoppingEvent/app/src/main/java/com/example/shoppingevent/data/ShoppingDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.shoppingevent.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.shoppingevent.data.daos.ShoppingEventDao
+import com.example.shoppingevent.data.daos.ShoppingItemDao
+import com.example.shoppingevent.data.entities.ShoppingEvent
+import com.example.shoppingevent.data.entities.ShoppingItem
+
+
+@Database(entities = [ShoppingEvent::class, ShoppingItem::class], version = 1)
+abstract class ShoppingDatabase : RoomDatabase() {
+    abstract fun shoppingEventDao(): ShoppingEventDao
+    abstract fun shoppingItemDao(): ShoppingItemDao
+
+    companion object {
+        const val DATABASE_NAME = "shopping_database"
+
+        @Volatile
+        private var Instance: ShoppingDatabase? = null
+
+        fun getDatabase(context: Context): ShoppingDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    ShoppingDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+            }
+        }
+    }
+}
