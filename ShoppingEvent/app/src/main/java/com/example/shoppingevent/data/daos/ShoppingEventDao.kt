27a272188af097e70b0ea1efa153ab02addfc48d
jinwook song <wlsdnr129@naver.com>
2025-02-15 12:35:05 +0900

    feat(query): get event and all items

--- ShoppingEvent/app/src/main/java/com/example/shoppingevent/data/daos/ShoppingEventDao.kt
+++ ShoppingEvent/app/src/main/java/com/example/shoppingevent/data/daos/ShoppingEventDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.example.shoppingevent.data.entities.ShoppingEvent
+import com.example.shoppingevent.data.entities.ShoppingItem
 import kotlinx.coroutines.flow.Flow
 
 
@@ -23,4 +24,15 @@ interface ShoppingEventDao {
     @Query("SELECT * FROM shopping_events")
     fun getEvents(): Flow<List<ShoppingEvent>>
 
+    @Query(
+        """
+    SELECT se.*, 
+           (SELECT SUM(si.price + si.quantity) FROM shopping_items si WHERE si.event_id = se.id) AS total_cost, 
+           si.*
+    FROM shopping_events se 
+    LEFT JOIN shopping_items si ON se.id = si.event_id 
+    WHERE se.id = :id
+    """
+    )
+    fun getEventAndItems(id: Long): Flow<Map<ShoppingEvent, List<ShoppingItem>>>
 }
